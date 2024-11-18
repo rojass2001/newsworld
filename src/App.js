2abac849cad7d9c05f6e44dabe917cf5c6324a8f
rojass2001 <rojascherian@gmail.com>
2024-11-18 15:02:51 +0530

    branch1add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import First from './Components/First';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
 //import { getData } from './Components/Api';
 import { useSelector,useDispatch } from 'react-redux';
-import Helo from './Components/Helo';
 import Header from './Components/Header';
 import Category from './Components/Category';
 import Mainpage from './Components/Mainpage';
